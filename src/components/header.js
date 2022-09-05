43b1d34bf4cc7ca01faa919b3396d45a3b0304ba
{fujitanao} <fujitanao.rmt@gmail.com>
2022-09-05 11:01:20 +0900

    Initial Commit

--- src/components/header.js
+++ src/components/header.js
@@ -6,17 +6,17 @@ const Header = () => {
   <>
     <header className={style.header}>
         <nav className={style.nav}>
-          <li className={style.spNav_item}><Link href="https://portfolio-fujita1000.vercel.app/#home"><a>HOME</a></Link></li>
-          <li className={style.spNav_item}><Link href="https://portfolio-fujita1000.vercel.app/#about"><a>ABOUT ME</a></Link></li>
-          <li className={style.spNav_item}><Link href="https://portfolio-fujita1000.vercel.app/#work"><a>WORK</a></Link></li>
-          <li className={style.spNav_item}><Link href="https://portfolio-fujita1000.vercel.app/#skill"><a>SKILL</a></Link></li>
+          <li className={style.spNav_item}><Link href="https://portfolio-fujita.vercel.app/#home"><a>HOME</a></Link></li>
+          <li className={style.spNav_item}><Link href="https://portfolio-fujita.vercel.app/#about"><a>ABOUT ME</a></Link></li>
+          <li className={style.spNav_item}><Link href="https://portfolio-fujita.vercel.app/#work"><a>WORK</a></Link></li>
+          <li className={style.spNav_item}><Link href="https://portfolio-fujita.vercel.app/#skill"><a>SKILL</a></Link></li>
           <li className={style.spNav_item}><Link href="./contact"><a>CONTACT</a></Link></li>
         </nav>
         <nav className={style.spNav}>
-          <li className={style.spNav_item}><Link href="https://portfolio-fujita1000.vercel.app/#home"><a>HOME</a></Link></li>
-          <li className={style.spNav_item}><Link href="https://portfolio-fujita1000.vercel.app/#about"><a>ABOUT ME</a></Link></li>
-          <li className={style.spNav_item}><Link href="https://portfolio-fujita1000.vercel.app/#work"><a>WORK</a></Link></li>
-          <li className={style.spNav_item}><Link href="https://portfolio-fujita1000.vercel.app/#skill"><a>SKILL</a></Link></li>
+          <li className={style.spNav_item}><Link href="https://portfolio-fujita.vercel.app/#home"><a>HOME</a></Link></li>
+          <li className={style.spNav_item}><Link href="https://portfolio-fujita.vercel.app/#about"><a>ABOUT ME</a></Link></li>
+          <li className={style.spNav_item}><Link href="https://portfolio-fujita.vercel.app/#work"><a>WORK</a></Link></li>
+          <li className={style.spNav_item}><Link href="https://portfolio-fujita.vercel.app/#skill"><a>SKILL</a></Link></li>
           <li className={style.spNav_item}><Link href="./contact"><a>CONTACT</a></Link></li>
         </nav>
       </header>
